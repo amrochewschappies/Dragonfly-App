e0aea81a6a377cc0c4f3de990b4c5fcaee5abc66
Amro <2592997@students.wits.ac.za>
2025-06-03 21:40:04 +0200

    Goddamn

---
+++ src/components/FavouritesBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FavouritesBackground() {
+  return (
+    <div>
+      <img src={process.env.PUBLIC_URL + "/Images/Events Background.jpg"} alt="Background Image of a beach" id='eventsBG-image' />
+    </div>
+  )
+}
+
+export default FavouritesBackground
